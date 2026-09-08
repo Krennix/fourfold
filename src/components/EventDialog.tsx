064a61877d6b1b063364f4666375d4bcdd1106bf
Krennix <142701586+Krennix@users.noreply.github.com>
2026-09-08 14:57:09 -0700

    calender updates

--- src/components/EventDialog.tsx
+++ src/components/EventDialog.tsx
@@ -116,6 +116,7 @@ export function EventDialog({
                   <option value="daily">Daily</option>
                   <option value="weekly">Weekly</option>
                   <option value="monthly">Monthly</option>
+                  <option value="yearly">Yearly</option>
                 </select>
               </div>
               {repeat !== 'none' && (
